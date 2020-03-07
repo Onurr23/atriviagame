e616aee5418b40ffdc0510e8a2508806a319453b
Onur Kara <60196841+Onurr23@users.noreply.github.com>
2020-03-07 14:51:02 +0300

    Build

---
+++ build/precache-manifest.efb4fe14477972d59748247a56ae41ee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95a95dc5e5a3bf867472bc9f6c5dffdb",
+    "url": "/atriviagame/index.html"
+  },
+  {
+    "revision": "e8b237ff4168e9b8f47c",
+    "url": "/atriviagame/static/css/main.a5d595ec.chunk.css"
+  },
+  {
+    "revision": "c6a1dbe94776c4d300cb",
+    "url": "/atriviagame/static/js/2.d5f72c49.chunk.js"
+  },
+  {
+    "revision": "c62b695abb80441dd5fa7b16a3dae5d5",
+    "url": "/atriviagame/static/js/2.d5f72c49.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8b237ff4168e9b8f47c",
+    "url": "/atriviagame/static/js/main.09d5e3fb.chunk.js"
+  },
+  {
+    "revision": "99f1e1894cda04ad45d6",
+    "url": "/atriviagame/static/js/runtime-main.ad7e2c41.js"
+  }
+]);
